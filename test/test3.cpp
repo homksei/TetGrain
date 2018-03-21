c83149af13d44e4eff2a9f742909b5bc985f36f4
Alexey Khomenko <hom.ksei@gmail.com>
2018-03-21 23:04:20 +0300

    add covert testing

--- test/test3.cpp
+++ test/test3.cpp
@@ -1,7 +1,26 @@
 #include "common.h"
+#include "grainmesh.h"
+#include "iotetgen.h"
+#include <iostream>
 
+
+using namespace grain;
 int main(int argc, char** argv)
 {
-
-	return 0;
+    GrainMesh gmsh;
+    GrainMesh gmsh1;
+    
+    // Geerate stub tetrahedron
+    gmsh.makeStubTetra();
+    
+    saveMFEM_meshFile("/Users/Dr.Iohanson/Desktop/grmesh.mesh", &gmsh);
+    // readNodeFile("/Users/Dr.Iohanson/Desktop/cube.node", &gmsh1);
+    // saveFaceFile("/Users/Dr.Iohanson/Desktop/testFace.mesh", &gmsh1);
+    int a = 5;
+    
+    
+    
+    
+    return 0;
 }
+
